815b28f12c2f16356d96c138d397e981b0b71b30
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-02-18 19:10:17 -0300

    Create compiladoreobytecode.java

---
+++ compiladoreobytecode.java
@@ -0,0 +1,13 @@
+//O compilador e o bytecode
+public class Programa02 {
+
+	public static void main(String[] args) {
+		int idade = 21;
+
+		System.out.println("Eu tenho " + idade + " anos");
+		
+		//javac Programa.java -> Programa.class -> java Programa
+		//código fonte (Programa.java) -> bytecode (Programa.class) -> executável
+	}
+
+}
