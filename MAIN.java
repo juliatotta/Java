ee526146224ab3964ec00acbe994075dbd9e8cd6
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-02-19 18:57:05 -0300

    Create MAIN.java

---
+++ MAIN.java
@@ -0,0 +1,11 @@
+//A função MAIN
+
+//Linguagem orientada a objeto
+
+public class Programa3 {
+//Essa é a função que torna a classe Java em programa Java
+	public static void main(String[] args) {
+
+	}
+
+}
