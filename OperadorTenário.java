383c29648387b3489b37db738f0c2359a5588343
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-02-20 17:16:21 -0300

    Create OperadorTenário.java

---
+++ OperadorTenário.java
@@ -0,0 +1,20 @@
+//Operador ternário
+public class Programa05 {
+//Se você precisar criar a função main() //programa java
+//main e no teclado Ctrl + barra de espaço
+	public static void main(String[] args) {
+		int valor = 10, numero;
+		
+		
+		if(valor < 0) {
+			numero = valor;
+		}
+		else {
+			numero = 7;
+		}
+		
+		//sysout + CTRL + barra de espaço
+		System.out.println(numero);
+		
+	}
+}
