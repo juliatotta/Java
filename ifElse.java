e09b42245671eb058d8f7011c2e91ce6cbd542c1
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-02-20 16:39:34 -0300

    Create ifElse.java

---
+++ ifElse.java
@@ -0,0 +1,16 @@
+//If, Else, If else
+public class Programa04 {
+
+	public static void main(String[] args) {
+		int numero = 6;
+		
+		if(numero < 5) 
+			System.out.println(numero + " menor que 5");
+			else
+				System.out.println(numero + " maior que 5");
+		
+		
+	
+	}
+
+}
