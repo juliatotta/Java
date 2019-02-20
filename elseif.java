e95ff7b7722f0f015f5728378b21c5c17936e2b8
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-02-20 16:45:10 -0300

    Create elseif.java

---
+++ elseif.java
@@ -0,0 +1,19 @@
+public class Programa04 {
+
+	public static void main(String[] args) {
+		int numero = 5;
+		
+		if(numero < 5) {
+			System.out.println(numero + " menor que 5");
+		}
+		else if(numero == 5) {
+			System.out.println(numero + " é igual a 5");
+		}
+		else {
+				System.out.println(numero + " maior que 5");
+		}
+		
+	
+	}
+
+}
