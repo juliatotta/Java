aa1d424fddb33f2b442f1962619baefa7dae2f2b
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-02-22 18:27:13 -0300

    Create doWhile.java

---
+++ doWhile.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+
+public class Programa08 {
+
+	public static void main(String[] args) {
+		
+		int idade;
+		String nome;
+		
+		
+		//Para receber dados do usuário via teclado
+		Scanner teclado = new Scanner(System.in);
+		
+		//Primeiro executa o bloco, depois faz a checagem 
+		do {
+		System.out.println("Informe seu nome: ");
+		nome = teclado.nextLine();
+		System.out.println("Informe sua idade: ");
+		//idade = teclado.nextInt(); bug 
+		idade = Integer.parseInt(teclado.nextLine());
+		
+		System.out.println(nome + " tem " + idade + " anos");
+		} while(idade > 0);
+		
+		teclado.close();
+	
+	}
+}
