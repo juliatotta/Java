66a500364c2f4d0e50efbfa22794dc8873d371ef
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-02-24 21:19:18 -0300

    Create Foreach.java

---
+++ Foreach.java
@@ -0,0 +1,14 @@
+//Foreach
+public class Programa10 {
+
+	public static void main(String[] args) {
+		String nome = "Xablau e Xena";
+		//Para cada um dos caracteres da String, imprima um caractere
+		for(char letra : nome.toCharArray()) {
+
+			System.out.print(letra);
+		}
+
+	}
+
+}
