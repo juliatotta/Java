4f9811b8b2a29bcb793f318a534f4bc20f1e8fb1
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-02-26 15:14:37 -0300

    Create TiposDeDadosNuméricos(Inteiros).java

---
+++ TiposDeDadosNuméricos(Inteiros).java
@@ -0,0 +1,21 @@
+//Tipos de dados Numéricos
+//Inteiros
+public class Programa11 {
+
+	public static void main(String[] args) {
+		//Tipos primários
+		int num1 = 4; //Inteiro
+		short num2 = 4; //Inteiro (curto/menor)
+		
+		//Tipos não primários
+		Integer num3 = 98;
+		Short num4 = 7;
+		
+		System.out.println("int -> Num1 = " + num1);
+		System.out.println("short -> Num2 = " + num2);
+		System.out.println("Integer -> Num3 = " + num3);
+		System.out.println("Short -> Num4 = " + num4);
+
+	}
+
+}
