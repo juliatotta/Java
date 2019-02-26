046858d2af0de30a86705c648a512041cd01f71a
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-02-26 15:24:14 -0300

    Create TiposDeDadosNuméricos(Reais).java

---
+++ TiposDeDadosNuméricos(Reais).java
@@ -0,0 +1,21 @@
+//Tipos de dados Numéricos
+//Reais
+public class Programa12 {
+
+	public static void main(String[] args) {
+		//Tipos primários
+		//por padrão, os números reais em Java são considerados double
+		float preco1 = 23.4f;
+		double preco2 = 23.4;
+
+		//Tipos não primários
+		Float preco3 = 44.5f;
+		Double preco4 = 44.5;
+
+		System.out.println("float -> preco1 = " + preco1);
+		System.out.println("double -> preco2 = " + preco2);
+		System.out.println("Float -> preco3 = " + preco3);
+		System.out.println("Double -> preco4 = " + preco4);
+	}
+
+}
