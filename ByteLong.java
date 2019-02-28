feb7d2e8eb860a595c699eb7d280e2d79153d8f9
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-02-27 21:46:20 -0300

    Create ByteLong.java

---
+++ ByteLong.java
@@ -0,0 +1,30 @@
+//Tipos de dados Numéricos
+//Inteiros
+public class Programa11 {
+
+	public static void main(String[] args) {
+		//Tipos primários
+		long num0 = 99;
+		int num1 = 4; //Inteiro
+		short num2 = 4; //Inteiro (curto/menor)
+		byte num3 = 4;
+	
+		
+		//Tipos não primários
+		Long num4 = (long) 999999;
+		Integer num5 = 98;
+		Short num6 = 7;
+		Byte num7 = 9;
+		
+		System.out.println("long -> Num0 = " + num0);
+		System.out.println("int -> Num1 = " + num1);
+		System.out.println("short -> Num2 = " + num2);
+		System.out.println("byte -> Num3 = " + num3);
+		System.out.println("Long -> Num4 = " + num4);
+		System.out.println("Integer -> Num5 = " + num5);
+		System.out.println("Short -> Num6 = " + num6);
+		System.out.println("Byte -> Num7 = " + num7);
+
+	}
+
+}
