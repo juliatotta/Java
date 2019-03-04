8cc92dd02131dda531b3784f5d2bbbd21ed5c3c2
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-03-04 19:25:23 -0300

    Create OperaçõesMatemáticas.java

---
+++ OperaçõesMatemáticas.java
@@ -0,0 +1,35 @@
+//Operações Matemáticas
+public class Programa15 {
+
+	public static void main(String[] args) {
+		int num1 = 5, num2 = 9, resultado;
+		float resultado2; 
+
+		//Soma
+		resultado = num1 + num2;
+		System.out.println("A soma de " + num1 + " + " + num2 + " = " + resultado);
+
+		//Subtração
+		resultado = num2 - num1;
+		System.out.println("A subtração de " + num2 + " - " + num1 + " = " + resultado);
+
+		//Subtração2
+		resultado = num1 - num2;
+		System.out.println("A subtração de " + num1 + " - " + num2 + " = " + resultado);
+
+		//Multiplicação
+		resultado = num1 * num2;
+		System.out.println("A multiplicação de " + num1 + " * " + num2 + " = " + resultado);
+		
+		//Divisão
+		resultado2 = (float)num2 / (float)num1;
+		System.out.println("A divisão de " + num2 + " / " + num1 + " = " + resultado2);
+		
+		//Divisão2
+		resultado2 = (float)num1 / (float)num2;
+		System.out.println("A divisão de " + num1 + " / " + num2 + " = " + resultado2);
+		
+		//
+	}
+
+}
