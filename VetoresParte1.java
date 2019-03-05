50fb87c37930d92d0f5ccb2212c3256219d35f52
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-03-04 22:41:40 -0300

    Create VetoresParte1.java

---
+++ VetoresParte1.java
@@ -0,0 +1,33 @@
+//Vetores parte1
+
+//Arrays (vetores uni-dimensionais)  
+
+/* 
+ int numero[5];
+
+ numero[0] = 1;
+ numero[1] = 3;
+ numero[2] = 5;
+ numero[3] = 7;
+ numero[4] = 9;
+ */
+
+public class Programa16 {
+
+	public static void main(String[] args) {
+		//Declarando e especificando o tamanho do vetor
+		int numeros[] = new int[5];
+
+		//Declarando, especificando o tamanho e inicializando o vetor
+		int outros_numeros[] = {1, 3, 5, 7, 9};
+		
+		
+		float valores[] = new float[5];
+		char caracteres[] = new char[20];
+		String nomes[] = new String[4];
+
+
+
+	}
+
+}
