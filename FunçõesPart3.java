7901912e622fb8a2c5d831dd2a5e5b0116b84a83
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-03-13 19:10:42 -0300

    Create FunçõesPart3.java

---
+++ FunçõesPart3.java
@@ -0,0 +1,16 @@
+//Ainda sobre funções
+public class Programa21 {
+	public static void main(String[] args) {
+		int valor1 = 6, valor2 = 4, resultado;
+
+		//resultado = soma(valor1, valor2);
+    //Declarar a variável resultado é uma opção, mas não é necessário
+    
+		System.out.println("O resultado é: " + soma(valor1, valor2));
+
+	}
+
+	static int soma(int v1,int v2) {
+		return v1 + v2;
+	}
+}
