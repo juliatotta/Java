20fb500d9fa830d7a181e847f732d2b544c53824
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-03-13 19:08:22 -0300

    Create Funçõespart2.java

---
+++ Funçõespart2.java
@@ -0,0 +1,15 @@
+//Ainda sobre funções
+public class Programa21 {
+	public static void main(String[] args) {
+		int valor1 = 6, valor2 = 4, resultado;
+
+		resultado = soma(valor1, valor2);
+
+		System.out.println("O resultado é: " + resultado);
+
+	}
+
+	static int soma(int v1,int v2) {
+		return v1 + v2;
+	}
+}
