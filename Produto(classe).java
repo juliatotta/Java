fee97d168a9502e448d9ff0f53293131c0cf651d
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-03-15 20:10:12 -0300

    Create Produto(classe).java

---
+++ Produto(classe).java
@@ -0,0 +1,14 @@
+//Classes 
+//Atributos
+//Métodos
+
+public class Produto {
+	String nome;
+	float   preco;
+	float desconto;
+	
+	//Método para aumentar o preço do produto em 10
+	void aumentar_preco() {
+		this.preco = this.preco + 10;
+	}
+}
