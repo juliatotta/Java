2175ecd7bf316de149a300f3c239fc04d69fe59c
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-03-16 20:34:23 -0300

    Update Pessoa(classe).java

--- Pessoa(classe).java
+++ Pessoa(classe).java
@@ -5,4 +5,25 @@
 public class Pessoa {
 	String nome, email;
 	int ano_nascimento;
+	
+	//Cosntrutor vazio
+	public Pessoa() {
+		
+	}
+	
+	//Construtor com parâmetro
+	public Pessoa(String nome, String email, int ano_nascimento) {
+		this.nome = nome;
+		this.email = email;
+		this.ano_nascimento = ano_nascimento;
+		
+	}
+	
+	void imprime_informacoes() {
+		System.out.println("Nome: " + this.nome);
+		System.out.println("E-mail: " + this.email);
+		System.out.println("Ano de Nascimento: " + this.ano_nascimento);
+	
+		System.out.println("");
+	}
 }
