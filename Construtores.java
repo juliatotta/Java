8488a62aa31e15c0a43a2ed8c2b820393bfaffb0
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-03-16 20:07:20 -0300

    Create Construtores.java

---
+++ Construtores.java
@@ -0,0 +1,67 @@
+
+public class Programa24 {
+
+	public static void main(String[] args) {
+		Pessoa pessoa1 = new Pessoa();
+
+		pessoa1.nome = "Diogo";
+		pessoa1.email ="diogo.dev@gmail.com";
+		pessoa1.ano_nascimento = 1987;
+
+		System.out.println("Nome: " + pessoa1.nome);
+		System.out.println("E-mail: " + pessoa1.email);
+		System.out.println("Ano de Nascimento: " + pessoa1.ano_nascimento);
+		
+		System.out.println("");
+
+		Pessoa pessoa2 = new Pessoa();
+
+		pessoa2.nome = "Xena";
+		pessoa2.email ="xenaxenotta@gmail.com";
+		pessoa2.ano_nascimento = 2017;
+
+		System.out.println("Nome: " + pessoa2.nome);
+		System.out.println("E-mail: " + pessoa2.email);
+		System.out.println("Ano de Nascimento: " + pessoa2.ano_nascimento);
+
+		System.out.println("");
+		
+		Pessoa pessoa3 = new Pessoa();
+
+		pessoa3.nome = "Xablau";
+		pessoa3.email ="xablauxablotta@gmail.com";
+		pessoa3.ano_nascimento = 2015;
+
+		System.out.println("Nome: " + pessoa3.nome);
+		System.out.println("E-mail: " + pessoa3.email);
+		System.out.println("Ano de Nascimento: " + pessoa3.ano_nascimento);
+
+		System.out.println("");
+		
+		Pessoa pessoa4 = new Pessoa();
+
+		pessoa4.nome = "Julia";
+		pessoa4.email ="tottajulia@gmail.com";
+		pessoa4.ano_nascimento = 1997;
+
+		System.out.println("Nome: " + pessoa4.nome);
+		System.out.println("E-mail: " + pessoa4.email);
+		System.out.println("Ano de Nascimento: " + pessoa4.ano_nascimento);
+
+		System.out.println("");
+		
+		Pessoa pessoa5 = new Pessoa();
+
+		pessoa5.nome = "Isaltina";
+		pessoa5.email ="tininhatotta@hotmail.com";
+		pessoa5.ano_nascimento = 1968;
+
+		System.out.println("Nome: " + pessoa5.nome);
+		System.out.println("E-mail: " + pessoa5.email);
+		System.out.println("Ano de Nascimento: " + pessoa5.ano_nascimento);
+
+
+
+	}
+
+}
