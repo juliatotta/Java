8ba759ff959bdb597003662708afef2af78cb0ae
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-03-16 20:18:48 -0300

    Create Construtorespart2.java

---
+++ Construtorespart2.java
@@ -0,0 +1,47 @@
+
+public class Programa24 {
+
+	public static void main(String[] args) {
+		Pessoa pessoa1 = new Pessoa("Diogo", "diogo.dev@gmail.com", 1987);
+
+		System.out.println("Nome: " + pessoa1.nome);
+		System.out.println("E-mail: " + pessoa1.email);
+		System.out.println("Ano de Nascimento: " + pessoa1.ano_nascimento);
+		
+		System.out.println("");
+
+		Pessoa pessoa2 = new Pessoa("Xena", "xenaxenotta@gmail.com", 2017);
+
+		System.out.println("Nome: " + pessoa2.nome);
+		System.out.println("E-mail: " + pessoa2.email);
+		System.out.println("Ano de Nascimento: " + pessoa2.ano_nascimento);
+
+		System.out.println("");
+		
+		Pessoa pessoa3 = new Pessoa("Xablau", "xablauxablotta@gmail.com", 2015);
+
+		System.out.println("Nome: " + pessoa3.nome);
+		System.out.println("E-mail: " + pessoa3.email);
+		System.out.println("Ano de Nascimento: " + pessoa3.ano_nascimento);
+
+		System.out.println("");
+		
+		Pessoa pessoa4 = new Pessoa("Julia", "tottajulia@gmail.com", 1997);
+
+		System.out.println("Nome: " + pessoa4.nome);
+		System.out.println("E-mail: " + pessoa4.email);
+		System.out.println("Ano de Nascimento: " + pessoa4.ano_nascimento);
+
+		System.out.println("");
+		
+		Pessoa pessoa5 = new Pessoa("Isaltina", "tininhatotta@hotmail.com", 1968);
+
+		System.out.println("Nome: " + pessoa5.nome);
+		System.out.println("E-mail: " + pessoa5.email);
+		System.out.println("Ano de Nascimento: " + pessoa5.ano_nascimento);
+
+
+
+	}
+
+}
