1a5b3e34bf1dc6c2bc30037fe1b012a5ceba46b0
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-03-16 20:33:25 -0300

    Update Construtorespart3.java

--- Construtorespart3.java
+++ Construtorespart3.java
@@ -5,27 +5,17 @@ public class Programa24 {
 		Pessoa pessoa1 = new Pessoa("Diogo", "diogo.dev@gmail.com", 1987);
 		pessoa1.imprime_informacoes();
 
-		System.out.println("");
-
 		Pessoa pessoa2 = new Pessoa("Xena", "xenaxenotta@gmail.com", 2017);
 		pessoa2.imprime_informacoes();
-
-		System.out.println("");
 
 		Pessoa pessoa3 = new Pessoa("Xablau", "xablauxablotta@gmail.com", 2015);
 		pessoa3.imprime_informacoes();
 
-		System.out.println("");
-
 		Pessoa pessoa4 = new Pessoa("Julia", "tottajulia@gmail.com", 1997);
 		pessoa4.imprime_informacoes();
 
-		System.out.println("");
-
 		Pessoa pessoa5 = new Pessoa("Isaltina", "tininhatotta@hotmail.com", 1968);
 		pessoa5.imprime_informacoes();
-
-
 
 	}
 
