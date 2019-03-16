1a0bc89de0350a07a3d11ac547f60d3a137306c5
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-03-16 20:25:07 -0300

    Create Construtorespart3.java

---
+++ Construtorespart3.java
@@ -0,0 +1,32 @@
+
+public class Programa24 {
+
+	public static void main(String[] args) {
+		Pessoa pessoa1 = new Pessoa("Diogo", "diogo.dev@gmail.com", 1987);
+		pessoa1.imprime_informacoes();
+
+		System.out.println("");
+
+		Pessoa pessoa2 = new Pessoa("Xena", "xenaxenotta@gmail.com", 2017);
+		pessoa2.imprime_informacoes();
+
+		System.out.println("");
+
+		Pessoa pessoa3 = new Pessoa("Xablau", "xablauxablotta@gmail.com", 2015);
+		pessoa3.imprime_informacoes();
+
+		System.out.println("");
+
+		Pessoa pessoa4 = new Pessoa("Julia", "tottajulia@gmail.com", 1997);
+		pessoa4.imprime_informacoes();
+
+		System.out.println("");
+
+		Pessoa pessoa5 = new Pessoa("Isaltina", "tininhatotta@hotmail.com", 1968);
+		pessoa5.imprime_informacoes();
+
+
+
+	}
+
+}
