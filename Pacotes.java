c499ca48e393d2098d170c7dab501ecca12cfb99
Julia Totta <32882425+juliatotta@users.noreply.github.com>
2019-03-17 21:55:30 -0300

    Create Pacotes.java

---
+++ Pacotes.java
@@ -0,0 +1,18 @@
+package br.com.geekuniversity.secao01;
+//Pacotes
+
+/*
+- Pacotes servem para organizar o codigo gonte do projeto
+- Pacotes indicam onde determinados arquivo do projeto se encontra
+- Os nomes de pacotes devem ser em letras minusculas, sem espaço, sem caractere especial, sem acentuação
+- Por convenção, é utilizado nos nomes de pacotes o domínio da empresa criadora do projeto ao contrário, sendo a última parte do nome o especificador do pacote.
+
+ */
+public class Programa23 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
